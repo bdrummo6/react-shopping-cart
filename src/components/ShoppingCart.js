0475e673c425086d36edb657280465e6318bb1be
bdrummo6 <bradley.drummonds@gmail.com>
2020-05-02 21:10:18 -0400

    Update ShoppingCart.js

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import CartContext from "../contexts/CartContext";
 
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
+	const contextValue = useContext(CartContext);
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return contextValue.cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
 	return (
-		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
-			))}
+		<CartContext.Consumer>
+			{context => (
+				<div className="shopping-cart">
+					{context.cart.map(item => (
+						<Item key={item.id} {...item} />
+					))}
 
-			<div className="shopping-cart__checkout">
-				<p>Total: ${getCartTotal()}</p>
-				<button>Checkout</button>
-			</div>
-		</div>
+					<div className="shopping-cart__checkout">
+						<p>Total: ${getCartTotal()}</p>
+						<button>Checkout</button>
+					</div>
+				</div>
+			)}
+		</CartContext.Consumer>
 	);
 };
 
